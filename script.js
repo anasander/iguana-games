bbecc5e667c9c3820a5b2ea9a3ea0d7d65b8c60c
Ana Sander <anagsander@gmail.com>
2022-09-23 17:58:02 -0300

    banner animacao init

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ function abrirMenu() {
   btnMenu.addEventListener("touchstart", toggleMenu);
 }
 abrirMenu();
+
+// SLIDESHOW
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+// ANIMAÇÃO
+var slideIndex = 0;
+
+function changeSlide() {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(changeSlide, 10000);
+}
+changeSlide();
